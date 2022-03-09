621871620dc57ae7c1ae0f82572c606627309f56
EC2 Default User <ec2-user@ip-172-31-45-251.us-east-2.compute.internal>
2022-03-09 10:34:47 +0000

    Laravel08課題

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,3 +11,9 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
+public function add()
+  {
+      return view('admin.news.create');
+  }
+
+}
