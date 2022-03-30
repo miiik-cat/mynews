92fa65db50c283f6e99b9864b2f8eab1c285d218
EC2 Default User <ec2-user@ip-172-31-45-251.us-east-2.compute.internal>
2022-03-30 01:53:04 +0000

    １３課題提出

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-});
 
 //3.「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください
 Route::get('XXX', 'AAAController@bbb');
@@ -24,10 +21,19 @@ Route::get('XXX', 'AAAController@bbb');
 //4.web.phpを編集して、admin/profile/create にアクセスしたら ProfileController の add Action に,
 //admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
-    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+Route::group(['prefix' => 'admin','middleware' => 'auth'], function() {
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::post('profile/create', 'Admin\ProfileController@create');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::post('profile/edit', 'Admin\ProfileController@update');
 });
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+    
+ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+     Route::get('news/create', 'Admin\NewsController@add');
+     Route::post('news/create', 'Admin\NewsController@create'); # 追記
+});
