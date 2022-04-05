9a4df6eef0ed065a7d5f7a87e4bc52c7c924427e
EC2 Default User <ec2-user@ip-172-31-45-251.us-east-2.compute.internal>
2022-04-05 17:02:00 +0000

    課題14

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Profile;
 
 class ProfileController extends Controller
 {
@@ -23,6 +24,30 @@ class ProfileController extends Controller
 
     public function update()
     {
+    $this->validate($request, Profile::$rules);
+    
+        $profile = new Profile;
+        $form = $request->all();
+        
+      // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+      if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $news->image_path = basename($path);
+      } else {
+          $news->image_path = null;
+      }
+      
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      // フォームから送信されてきたimageを削除する
+      unset($form['image']);
+      
+      // データベースに保存する
+      $profile->fill($form);
+      $profile->save();
+        
+        
+        
         return redirect('admin/profile/edit');
     }
 }
